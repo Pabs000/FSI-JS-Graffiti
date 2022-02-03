751ad48ea16c73c589c62b5980958a2721de8f97
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-02-02 19:13:39 -0800

    changed the dog alignment

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
+...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.images.length; i++){
+    dogImages[i].style.style.borderRadius = '50px'
+}
+
+let body = document.querySelector('dog-name')
+header.style.textAlign = "left"
+
+for(let i = 0; i < dogName.Name.length; i++){
+    dogNames[i].length.dogNames.textAlign = "left"
+
+}
